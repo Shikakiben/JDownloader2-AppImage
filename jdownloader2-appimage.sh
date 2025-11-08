a384a8869021aefa78c31cb9798f3b8ab1b074ba
Shikakiben <shikakiben@example.com>
2025-11-08 12:58:19 +0100

    fix: utiliser megacmd/megadl pour téléchargement Mega

--- jdownloader2-appimage.sh
+++ jdownloader2-appimage.sh
@@ -15,7 +15,7 @@ tar -xzf OpenJDK.tar.gz --strip-components=1 -C jd2/jre
 
 # Téléchargement JDownloader2
 mkdir -p jd2
-megatools dl --no-ask-password --path=jd2 "https://mega.nz/file/qU1TCYjL#g8a05FYWPGyqFgy1QWQ9L5nScEOmOU6iZh1eDhSn-sk"
+megadl --path=jd2 "https://mega.nz/file/qU1TCYjL#g8a05FYWPGyqFgy1QWQ9L5nScEOmOU6iZh1eDhSn-sk"
 INSTALL4J_JAVA_HOME="$PWD/jd2/jre" xvfb-run -a bash jd2/JDownloader2Setup_unix_nojre.sh -q -dir "${PWD}/jd2"
 
 # Préparation AppDir
