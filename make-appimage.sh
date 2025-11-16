b8634339d0e5a11fbb01b03e22594d03bb3bf635
Shikakiben <108907095+Shikakiben@users.noreply.github.com>
2025-11-16 12:16:16 +0100

    Remove JD2_APPIMAGE_BUILD export
    Remove unnecessary export of JD2_APPIMAGE_BUILD variable.

--- make-appimage.sh
+++ make-appimage.sh
@@ -7,7 +7,6 @@ VERSION=$(pacman -Q JDownloader2 | awk '{print $2; exit}') # example command to 
 export ARCH VERSION
 export OUTPATH=./dist
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export JD2_APPIMAGE_BUILD=1
 
 # Make the AppImage with uruntime
 quick-sharun --make-appimage
